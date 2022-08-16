fc89c5c1394b96d1c2fcb779366c9ddbe4a5e838
Ben Allen <benalleng@gmail.com>
2022-08-16 13:30:49 -0400

    WIP: complete API request portion

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ const API_KEY = '4f61dc9d759dcaa5d4579250c1e2e3d9';
 const $input = $('input[type="text"]');
 let $button = $('button');
 let hungers = [];
+let excluded = [];
 // let q = '';
 // let diet = '';
 // let cuisineType = '';
@@ -19,84 +20,80 @@ let hungers = [];
 $input.on('keydown', handleEnterPress);
 
 $(document).on('click', '#submit', function() {
+  excluded = hungers.slice(6);
   AJAXPush();
 });
 
 
 // functions
 
-// const promise = $.ajax(`${RECIPE_URL}&app_id=${API_ID}&app_key=${API_KEY}&random=true`);
-    
-//     promise.then(
-//       (data) => {
-//         console.log(data);
-//     },
-//     (error) => {
-//         console.log('bad request: ', error);
-//     }
-//     );
 
-function handleEnterPress(event) {
-    if (event.keyCode === 13) {
-      handleTextInput(event);
-    }
-}
-
-function handleTextInput(event) {
-  event && event.preventDefault();
-
-  const want = $input.val();
-  
-  if(!want) return;
-
-  $input.focus();
-  console.log(want);
-  
-  // if(!want) return;
-  // if (hungers.length == 0) {
-  //   q = $input.val();
-  // } else if(hungers.length == 1) { 
-  //   cuisineType = $input.val();
-  // } else if(hungers.legnth == 2) {
-  //   mealType = $input.val();
-  // } else if(hungers.length = 3) {
-  //   dishType = $input.val(); 
-  // } else if(hungers.length = 4) {
-  //   excluded = $input.val();
-  // }
-
-  hungers.push($input.val());
-  
-  $input.val('');
-  
-  console.log(hungers);
-  inputChanger();
-}
-
+      
+      function handleEnterPress(event) {
+        if (event.keyCode === 13) {
+          handleTextInput(event);
+        }
+      }
+      
+      function handleTextInput(event) {
+        event && event.preventDefault();
+        
+        const want = $input.val();
+        
+        if(!want) return;
+        
+        $input.focus();
+        console.log(want);
+                  
+        hungers.push($input.val());
+                  
+        $input.val('');
+                  
+        console.log(hungers);
+                  
+        inputChanger();
+      }
+                
 function inputChanger() {
   if (hungers.length == 0) {
-    $('input:text').attr('placeholder','Hungry');
-  } else if (hungers.length == 1) {
-    $('input:text').attr('placeholder','Italian');
-  } else if (hungers.length == 2) {
-    $('input:text').attr('placeholder','Vegan');
-  } else if (hungers.length == 3) {
-    $('input:text').attr('placeholder','Dinner');
-  } else if (hungers.length == 4) {
-    $('input:text').attr('placeholder','Main course');
-  } else if (hungers.length == 5) {
-    $('input:text').attr('placeholder','Brussel Sprouts YUCK!');
-    let $input = $('<input type="button" id="submit" value="Get Recipe" />');
-    $input.appendTo($("main"));
-  } else {
+        $('input:text').attr('placeholder','Hungry');
+    } else if (hungers.length == 1) {
+        $('input:text').attr('placeholder','Italian');
+    } else if (hungers.length == 2) {
+        $('input:text').attr('placeholder','Vegan');
+    } else if (hungers.length == 3) {
+        $('input:text').attr('placeholder','Dinner');
+    } else if (hungers.length == 4) {
+        $('input:text').attr('placeholder','Main course');
+    } else if (hungers.length == 5) {
+        $('input:text').attr('placeholder','Brussel Sprouts YUCK!');
+        let $input = $('<input type="button" id="submit" value="Get Recipe" />');
+        $input.appendTo($("main"));
+    } else {
     return;
   }
 }
 
 function AJAXPush() {
-  let promise = (`${RECIPE_URL}&q=${hungers[0]}&app_id=${API_ID}&app_key=${API_KEY}&diet=${hungers[1]}&health=${hungers[2]}&cuisineType=${hungers[3]}&mealType=${hungers[4]}&dishType=${hungers[5]}&excluded=${hungers[6]}`)
-  console.log(promise);
+  // excludedJoin = 
+  let shifter = excluded.map(function(gross) {
+    return `&excluded=${gross}`;
+  });
+
+  let joiner = shifter.join('')
+  
+  let promise = $.ajax(`${RECIPE_URL}&q=${hungers[0]}&app_id=${API_ID}&app_key=${API_KEY}&diet=${hungers[1]}&health=${hungers[2]}&cuisineType=${hungers[3]}&mealType=${hungers[4]}&dishType=${hungers[5]}${joiner}&random=true`);
+  // console.log(promise);
+    promise.then(
+    (data) => {
+        console.log(data);
+        },
+    (error) => {
+      console.log('bad request: ', error);
+    }
+  );
 }
+
 
 
 
